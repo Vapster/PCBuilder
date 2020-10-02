a79a44187822e2f60b4ab477a4cc1186516d69a0
Vapster <varshilpatel1498@gmail.com>
2020-10-01 21:48:10 -0400

    Delete mongoose.js

--- backend/src/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectionURL = "mongodb+srv://pcbBackend:Varshil007.@cluster0.bhomg.mongodb.net/pcbData?retryWrites=true&w=majority"
-mongoose.connect(connectionURL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true })
-
